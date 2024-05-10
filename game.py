9393a72ed40070e503ff4782e095ef7b630798e2
DebandeAurelienHeh <aurelien.debande@std.heh.be>
2024-05-10 16:39:06 +0200

    Update game.py

--- game.py
+++ game.py
@@ -6,13 +6,8 @@ class Orelient:
   pass
 
 class Sony:
-<<<<<<< HEAD
   pass
 
 
 class aurelien_le_plus_Beau:
   pass
-=======
-  print("Helldivers 2")
-  pass
->>>>>>> 9915b319ffbf55be3d1de3522c655cfa418f8754
